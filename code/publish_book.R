f5a3af0b30df9728beb0fa357b9e0c7ebec642eb
Marc Teunis <marc.teunis@hu.nl>
2020-12-19 11:28:23 +0100

    add entry point

--- code/publish_book.R
+++ code/publish_book.R
@@ -3,16 +3,11 @@ dir_out <- here::here("reader_tlsc-dfsb26v_workflows")
 bookdown::render_book(
   input = here::here(),
   output_dir = dir_out)
-
-# bookdown::publish_book(
-#   name = "Workflows_reader",
-#   account = "rstudio",
-#   server = "https://splendid-crocodile.eval.rstudio.com",
-#   render = c("local")
-# )
-
 ## create manifest for github continous integration updates the bookdown website
 ## upon push to master
 
-rsconnect::writeManifest()
-
+rsconnect::writeManifest(
+  appPrimaryDoc = here::here(
+    dir_out,
+    "index.html"),
+  contentCategory = "site")
